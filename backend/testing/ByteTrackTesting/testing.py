c764089a0a00a8ee9976d6aadd2e1e836ddb670c
Sriram Koukuntla <sriram.koukuntla@gmail.com>
2025-12-26 23:13:06 -0600

    testing files

---
+++ backend/testing/ByteTrackTesting/testing.py
@@ -0,0 +1,32 @@
+from ultralytics import YOLO
+import supervision as sv
+from supervision.assets import download_assets, VideoAssets
+
+
+download_assets(VideoAssets.PEOPLE_WALKING)
+
+model = YOLO("yolo11n.pt")  # local file
+tracker = sv.ByteTrack()
+
+box_annotator = sv.BoxAnnotator()
+label_annotator = sv.LabelAnnotator()
+
+def callback(frame, _):
+    results = model(frame)[0]
+    detections = sv.Detections.from_ultralytics(results)
+    detections = tracker.update_with_detections(detections)
+
+    labels = [
+        f"#{tid}"
+        for tid in detections.tracker_id
+    ]
+
+    frame = box_annotator.annotate(frame, detections)
+    frame = label_annotator.annotate(frame, detections, labels)
+    return frame
+
+sv.process_video(
+    source_path="people-walking.mp4",
+    target_path="output.mp4",
+    callback=callback
+)
